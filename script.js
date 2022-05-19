ee734ca1d2a16e82c4d31a234790043086b08cc2
Kupi403 <michal.kupidura2003@wp.pl>
2022-05-19 23:11:30 +0200

    Added function which completes the task

--- script.js
+++ script.js
@@ -7,49 +7,36 @@ const todoTemplate = document.querySelector('.todo-template')
 
 let ID = 0
 
+
 const createNewTask = () => {
 	if (input.value !== '') {
 		const newTodo = todoTemplate.content.cloneNode(true)
+
 		const todoBox = newTodo.querySelector('.todo')
 		const todoText = newTodo.querySelector('.todo-text')
 
-		changeTodoOptions(todoBox)
-
-		// const todoCompleteBtn = newTodo.querySelector('.todo-complete')
-		// const todoEditBtn = newTodo.querySelector('.todo-edit')
-		// const todoDeleteBtn = newTodo.querySelector('.todo-delete')
 		todoBox.setAttribute('id', ID)
 		todoText.textContent = input.value
 		todoContainer.append(todoBox)
 
-		ID++
 		changeTodoOptions(todoBox)
-		// todoCompleteBtn.addEventListener('click', completeTask)
-		// todoEditBtn.addEventListener('click', editTask)
-		// todoDeleteBtn.addEventListener('click', deleteTask)
+
+		ID++
 	} else {
 		errorBox.textContent = 'Enter your to-do'
 	}
 }
 
 const changeTodoOptions = todoBox => {
-	console.log(todoBox.querySelector('.todo-complete'))
-
 	const todoCompleteBtn = todoBox.querySelector('.todo-complete')
 	const todoEditBtn = todoBox.querySelector('.todo-edit')
 	const todoDeleteBtn = todoBox.querySelector('.todo-delete')
 
-	console.log(todoCompleteBtn)
 	todoCompleteBtn.addEventListener('click', () => {
-		console.log('gowno')
+		todoBox.classList.toggle('todo-done')
+		todoCompleteBtn.textContent == '✅' ? todoCompleteBtn.textContent = '✔' : todoCompleteBtn.textContent = '✅'
 	})
 
-	console.log(todoBox)
-
-	// todoCompleteBtn.addEventListener('click', () => {
-	// 	todoBox.classList.toggle('todo-done')
-	//     console.log('jelo');
-	// })
 	// todoEditBtn.addEventListener('click', editTask)
 	// todoDeleteBtn.addEventListener('click', deleteTask)
 }
